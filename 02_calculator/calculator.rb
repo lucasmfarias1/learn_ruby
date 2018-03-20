697320d9bc28573891c94e0c9b9e1c88a76d0c4b
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-03-20 18:11:03 -0300

    Solve exercises 00, 01 and 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,25 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  return 0 if array == []
+  array.reduce(:+)
+end
+
+def multiply(*args)
+  args.reduce(:*)
+end
+
+def power(base, exp)
+  base ** exp
+end
+
+def factorial(n)
+  return 0 if n <= 0
+  (1..n).reduce(:*)
+end
