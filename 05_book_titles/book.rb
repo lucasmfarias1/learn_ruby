a7b6c3a92868c9926d420294a67422b0eece8038
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-03-21 18:56:08 -0300

    Solve exercise 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+  attr_accessor :title
+  LITTLE_WORDS = ['and', 'the', 'in', 'of', 'a', 'an']
+
+  def title=(title)
+    @title = title.split(' ').map do |word|
+      LITTLE_WORDS.include?(word) ? word : word.capitalize
+    end
+    @title.first.capitalize!
+    @title = @title.join(' ')
+  end
 end
