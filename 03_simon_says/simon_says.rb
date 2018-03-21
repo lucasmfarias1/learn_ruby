6edd4f912f880527e52a745444ea1624044cf906
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-03-21 18:35:05 -0300

    Solve exercises 03 and 04

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
-#write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, times = 2)
+  ([string] * times).join(' ')
+end
+
+def start_of_word(string, number_of_characters)
+  string[0...number_of_characters]
+end
+
+def first_word(string)
+  string.split(' ').first
+end
+
+LITTLE_WORDS = ['and', 'over', 'the']
+def titleize(string)
+  words = string.split(' ').map do |word|
+    LITTLE_WORDS.include?(word) ? word : word.capitalize
+  end
+  words.first.capitalize!
+  words.join(' ')
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,16 @@
-#write your code here
+def translate(string)
+  words = string.split(' ').map do |word|
+    if word.match?(/\A[aeiou]/)
+      word << 'ay'
+    else
+      v = first_vowel(word)
+      word[v..-1] << word[0...v] << 'ay'
+    end
+  end.join(' ')
+end
+
+def first_vowel(word)
+  return 2 if word.match?(/^qu/)
+  return 3 if word.match?(/^[^aeiou]qu/)
+  return word.index(/[aeiou]/)
+end
